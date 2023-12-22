11a303a39cc46206168f836724654a010bce8667
Hikari Kobe <h.kobe712@gmail.com>
2023-12-21 17:25:22 -0800

    feat: added footer

---
+++ app/[locale]/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { roboto } from '../lib/fonts';
+
+const Footer = () => {
+  return (
+    <div
+      className={`${roboto.className} pb-5 pt-10 text-center text-[12px] leading-relaxed text-gray-dark`}
+    >
+      © 2023 | Designed & Coded By Hikari Kobe
+    </div>
+  );
+};
+
+export default Footer;
