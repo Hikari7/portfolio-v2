31801cd579a8d4cc73b4e76b33ca50a6cb3770c3
Hikari Kobe <h.kobe712@gmail.com>
2024-01-05 15:03:52 -0800

    feat: tooltips

--- app/[locale]/components/SocialMediaDiv.tsx
+++ app/[locale]/components/SocialMediaDiv.tsx
@@ -1,40 +1,51 @@
+'use client';
+
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FaXTwitter, FaLinkedin } from 'react-icons/fa6';
 import ExternalLink from '../ui/Common/ExternakLink';
 import { IoMdMail } from 'react-icons/io';
+import Tooltip from '../ui/Common/Tooltip';
 
 const SocialMediaDiv = () => {
   return (
     <div className='fixed bottom-8 right-7 flex flex-col gap-3'>
-      <ExternalLink href='https://github.com/Hikari7'>
-        <FaGithub
-          size={28}
-          className='hover:opacity-60'
-          style={{ color: '#28232F' }}
-        />
-      </ExternalLink>
-      <ExternalLink href='https://twitter.com/hk_Vancouver'>
-        <FaXTwitter
-          size={28}
-          className='hover:opacity-60'
-          style={{ color: '#28232F' }}
-        />
-      </ExternalLink>
-      <ExternalLink href='https://www.linkedin.com/in/hikari-kobe/'>
-        <FaLinkedin
-          size={28}
-          className='hover:opacity-60'
-          style={{ color: '#28232F' }}
-        />
-      </ExternalLink>
-      <a href='mailto:h.kobe712@gmail.com' className=''>
-        <IoMdMail
-          size={28}
-          className='hover:opacity-60'
-          style={{ color: '#28232F' }}
-        />
-      </a>
+      <Tooltip label={'GitHub'}>
+        <ExternalLink href='https://github.com/Hikari7'>
+          <FaGithub
+            size={28}
+            className='hover:opacity-60'
+            style={{ color: '#28232F' }}
+          />
+        </ExternalLink>
+      </Tooltip>
+      <Tooltip label={'X'}>
+        <ExternalLink href='https://twitter.com/hk_Vancouver'>
+          <FaXTwitter
+            size={28}
+            className='hover:opacity-60'
+            style={{ color: '#28232F' }}
+          />
+        </ExternalLink>
+      </Tooltip>
+      <Tooltip label={'LinkedIn'}>
+        <ExternalLink href='https://www.linkedin.com/in/hikari-kobe/'>
+          <FaLinkedin
+            size={28}
+            className='hover:opacity-60'
+            style={{ color: '#28232F' }}
+          />
+        </ExternalLink>
+      </Tooltip>
+      <Tooltip label={'Mail'}>
+        <a href='mailto:h.kobe712@gmail.com'>
+          <IoMdMail
+            size={28}
+            className='hover:opacity-60'
+            style={{ color: '#28232F' }}
+          />
+        </a>
+      </Tooltip>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -42,10 +42,15 @@ const config: Config = {
           from: { opacity: '0', transform: 'translateY(-2px)' },
           to: { opacity: '1', transform: 'translateY(0)' },
         },
+        slideLeftAndFade: {
+          from: { opacity: '0', transform: 'translateX(2px)' },
+          to: { opacity: '1', transform: 'translateX(0)' },
+        },
       },
     },
     animation: {
-      slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+      slideDownAndFade: 'slideDownAndFade 300ms cubic-bezier(0.16, 1, 0.3, 1)',
+      slideLeftAndFade: 'slideLeftAndFade 300ms cubic-bezier(0.16, 1, 0.3, 1)',
     },
     plugins: [],
   },
